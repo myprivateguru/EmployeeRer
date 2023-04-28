3b6043c87d76fc2f5138006c1a6a42aca3aaeeb7
Somvanshi <11365@intelizign.com>
2023-04-28 18:36:16 +0530

    Daily Streak phase 1 complete

--- src/main/java/com/jobportal/brs/security/form/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/jobportal/brs/security/form/CustomAuthenticationSuccessHandler.java
@@ -1,9 +1,14 @@
 package com.jobportal.brs.security.form;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.jobportal.brs.dto.model.user.DailyStreakDto;
+import com.jobportal.brs.service.DailyStreakService;
+import com.jobportal.brs.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +20,11 @@ import java.io.IOException;
  */
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private DailyStreakService dailyStreakService;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
@@ -22,6 +32,8 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         response.setStatus(HttpServletResponse.SC_OK);
         for (GrantedAuthority auth : authentication.getAuthorities()) {
             if ("ADMIN".equals(auth.getAuthority())) {
+            	userService.SetLoginHistory();
+            	DailyStreakDto streak = dailyStreakService.getDailyStreak();
                 response.sendRedirect("/dashboard");
             }
         }
